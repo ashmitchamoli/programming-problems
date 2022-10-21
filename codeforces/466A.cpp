4505ebb1db567770e6a6cff0c201d96677692eec
ashmitchamoli <ashmitchamoli@outlook.com>
2022-10-21 11:20:46 +0530

    init

---
+++ codeforces/466A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    // lets say 'x' b tickets - mx le liye n-mx bacha
+    // bx + (n-mx)a = x(b-ma) + an -> minimimse this brute force lol
+    int p = b - m*a;
+    int q = a*n;
+    int x = 0;
+    int ans = INT_MAX;
+    while(m*x <= n)
+    {
+        ans = min(ans, x*p + q);
+        x++;
+    }
+    cout << min(ans, b*x) << '\n';
+}
