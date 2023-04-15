90865f7a463eabc97f04c55c6fe67257195f24e4
ashmitchamoli <ashmitchamoli@outlook.com>
2023-04-15 09:09:53 +0530

    solved cses 1091

--- CSES/Sorting and Searching/1091.cpp
+++ CSES/Sorting and Searching/1091.cpp
@@ -8,33 +8,30 @@ int main() {
     int n, m; cin >> n >> m;
     vector<ll> a(n), b(m);
     vector<pair<ll, int>> arr;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < m; i++) {
-        cin >> b[i];
-        arr.push_back({b[i], i});
+    multiset<int> tickets;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        tickets.insert(a[i]);
     }
-
-    sort(a.begin(), a.end());
-    sort(arr.begin(), arr.end());
-
-    for(int i = 0; i < m; i++) b[i] = -1;
-
-    int i = n-1, j = m-1;
-    while(i >= 0 && j >= 0) {
-        if(a[i] <= arr[j].first) {
-            b[arr[j].second] = a[i];
-            i--; j--;
+    for(int i = 0; i < m; i++) cin >> b[i];
+    
+    for(int i = 0; i < m; i++) {
+        auto it = tickets.upper_bound(b[i]);
+        if(it == tickets.begin()) {
+            cout << -1 << "\n";
         }
         else {
-            i--;
+            cout << *(--it) << "\n";
+            tickets.erase(it);
         }
     }
-    for(int i = 0; i < m; i++) cout << b[i] << "\n";
 }
 /*
+input
 10 10
-9 9 8 6 6 6 6 3 3 2
-9 9 6 5 5 4 3 3 3 2 
+9 3 9 6 6 8 6 2 6 3
+9 5 4 6 3 9 3 3 5 2
 
+9 3 3 6 2 9 -1 -1 -1 -1
 b[i] = {9, 9, 6, 3, 3, 2, -, -, -, -}
 */
